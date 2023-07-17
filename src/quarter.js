404f4da9ee4a1e5b4dd02db4bfd349c7a12c0437
Ryan Moultrup <rmoultrup@gmail.com>
2023-07-16 19:06:31 -0500

    Cleans code

--- src/quarter.js
+++ src/quarter.js
@@ -4,10 +4,10 @@ export default (data) => {
     const Q3 = Math.floor(3 * data.length / 4);
 
     return data.reduce((partitions, currentVal, currentIndex) => {
-            if (currentIndex < Q1) partitions[0].push(currentVal);
-            else if (currentIndex < Q2) partitions[1].push(currentVal);
-            else if (currentIndex < Q3) partitions[2].push(currentVal);
-            else partitions[3].push(currentVal);
-            return partitions;
-        }, [[], [], [], []]);
+        if (currentIndex < Q1) partitions[0].push(currentVal);
+        else if (currentIndex < Q2) partitions[1].push(currentVal);
+        else if (currentIndex < Q3) partitions[2].push(currentVal);
+        else partitions[3].push(currentVal);
+        return partitions;
+    }, [[], [], [], []]);
 };
